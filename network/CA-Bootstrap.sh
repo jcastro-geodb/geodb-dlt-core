83e5710bb71ad1d39becf7d59c311261bdef2a4c
Rafael <rafael@geodb.com>
2019-06-04 11:15:37 +0200

    Root CA Server first implementation

--- network/CA-Bootstrap.sh
+++ network/CA-Bootstrap.sh
@@ -13,14 +13,13 @@
 #set -e
 
 # Organization info where each line is of the form:
-#    <type>:<orgName>:<rootCAPort>:<intermediateCAPort>:<numOrderersOrPeers>
 #    <orgName>:<numPeers>:<numOrderers>:<rootCAPort>:<intermediateCAPort>
 ORGS="\
    geodb.com:1:1:7100:7101 \
 "
 
 # If true, uses both a root and intermediate CA
-INTERMEDIATE_CA=false
+INTERMEDIATE_CA=true
 
 # If true, recreate crypto if it already exists
 RECREATE=true
