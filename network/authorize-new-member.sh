5293ce71a1e10e588a723b17f84be3986f320878
Rafael <rafael@geodb.com>
2019-07-05 11:22:30 +0200

    configtxlator flow control

--- network/authorize-new-member.sh
+++ network/authorize-new-member.sh
@@ -1,8 +1,32 @@
 #!/bin/bash +x
 
+function startConfigtxlator {
+
+  if lsof -Pi :7059 -sTCP:LISTEN -t >/dev/null ; then
+      fatal "Could not reserve port for configtxlator: TCP 7059 is in use"
+  else
+      configtxlator start &
+  fi
+
+  sleep 1s
+
+  if lsof -Pi :7059 -sTCP:LISTEN -t >/dev/null ; then
+      echo "configtxlator is now running on port TCP 7059"
+  else
+      fatal "Could not detect configtxlator on port TCP 7059"
+  fi
+}
+function stopConfigtxlator {
+  if lsof -Pi :7059 -sTCP:LISTEN -t >/dev/null ; then
+      echo "Stopping configtxlator"
+      fuser -k 7059/tcp
+  fi
+}
+
 # Print a fatal error message and exit
 function fatal {
    echo "FATAL: $*"
+   stopConfigtxlator
    exit 1
 }
 
@@ -26,8 +50,8 @@ case $key in
     shift
     shift
     ;;
-    -c|--channel)
-    CHANNEL_NAME_LOCAL="$2"
+    -C|--channel)
+    CHANNEL_ID_LOCAL="$2"
     shift
     shift
     ;;
@@ -47,7 +71,7 @@ if [ -z "$ORDERER_URL" ]; then
   fatal "No orderer URL was specified"
 fi
 
-if [ -z "$CHANNEL_NAME_LOCAL" ]; then
+if [ -z "$CHANNEL_ID_LOCAL" ]; then
   fatal "No channel was specified"
 fi
 
@@ -55,9 +79,11 @@ if [ ! "$(docker ps -q -f name=$CLI)" ]; then
   fatal "The specified CLI was not found. Is your local peer node running?"
 fi
 
-configtxlator start &
+startConfigtxlator
 export CONFIGTXLATOR_URL=http://127.0.0.1:7059
-export CHANNEL_NAME=$CHANNEL_NAME_LOCAL
+export CHANNEL_ID=$CHANNEL_ID_LOCAL
 
 
-docker exec -it $CLI bash -c "peer channel fetch config ./channels/config_block.pb -o $ORDERER_URL -c ${CHANNEL_NAME}; chmod 755 ./channels/config_block.pb"
+docker exec -it $CLI bash -c "peer channel fetch config ./channels/config_block.pb -o $ORDERER_URL -c ${CHANNEL_ID}; chmod 755 ./channels/config_block.pb"
+
+stopConfigtxlator
